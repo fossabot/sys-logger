b75fe741a8f5495c95d473ceff7044fd36488d7a
Abhigyan Kumar <44155310+AbhigyanKumar@users.noreply.github.com>
2020-09-29 19:00:40 +0530

    🧪 for ✏

--- log_test.go
+++ log_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2020 Abhigan Kumar
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+https://www.eclipse.org/legal/epl-2.0/
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied. See the License for the specific language governing
+permissions and limitations under the License.
+*/
+
 package logger
 
 import (
@@ -16,9 +32,24 @@ func TestNewPencil(t *testing.T) {
 		want Pencil
 	}{
 		{
-			"Test case: A",
-			args{"./tests/mark.txt"},
-			Pencil{filePath: "./tests/mark.txt",},
+			"Alpha",
+			args{"./tests/log/alpha.txt"},
+			Pencil{filePath: "./tests/log/alpha.txt",},
+		},
+		{
+			"Beta",
+			args{"./tests/log/beta.txt"},
+			Pencil{filePath: "./tests/log/beta.txt",},
+		},
+		{
+			"Gamma",
+			args{"./tests/log/gamma.txt"},
+			Pencil{filePath: "./tests/log/gamma.txt",},
+		},
+		{
+			"Lambda",
+			args{"./tests/log/lambda.txt"},
+			Pencil{filePath: "./tests/log/lambda.txt",},
 		},
 	}
 	for _, tt := range tests {
